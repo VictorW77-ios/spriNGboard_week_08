99153aafe426794ceeefc29976f4bbb3ae6d13d8
victor_workspace <victor@aws_workspace>
2023-03-16 16:37:59 -0500

    update code + notes

--- src/game_lib/Game.cpp
+++ src/game_lib/Game.cpp
@@ -135,8 +135,8 @@ bool Game::handleCorrectAnswer ()
   }
   else
   {
-    // preserve existing bug behavior
-    string correct = inPenaltyBox[currentPlayer] ? "correct" : "corrent";
+    // fixed bug behavior
+    string correct = "correct"; 
     cout << "Answer was " << correct << "!!!!" << endl;
 
     purses[currentPlayer]++;
